eabac1ca921bc7ba97ab99ed50bfda8b8947e216
sdivyamit <82748261+sdivyamit@users.noreply.github.com>
2023-09-06 11:32:28 +0530

    Update Research.js

--- src/components/Sections/Dashboard/Research.js
+++ src/components/Sections/Dashboard/Research.js
@@ -64,7 +64,7 @@ const Research = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
            </svg>
           </button>
-        <span className="text-white font-semibold">Kindly Sign Up To Proceed Further \n No Credit Card Needed.</span>
+        <span className="text-white font-semibold">Kindly Sign Up To Proceed Further, No Credit Card Needed !.</span>
       </div>
       <button className="text-red-200 hover:text-red-100 transition duration-150" onClick={() => setShowFlashMessage(false)}>Close</button>
         </div>
