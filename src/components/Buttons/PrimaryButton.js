ed380f1ba150a0c88137270349018d43395b6fb4
sdivyamit <82748261+sdivyamit@users.noreply.github.com>
2023-09-12 15:57:31 +0530

    Delete src directory

--- src/components/Buttons/PrimaryButton.js
+++
@@ -1,6 +0,0 @@
-const PrimaryButton = ({text}) => {
-    return <button className="bg-primary text-white w-full min-w-fit h-12 p-2 rounded-md">
-        {text}
-    </button>
-}
-export default  PrimaryButton
